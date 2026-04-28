508b3c4bccb3eaa252105afe2db8b49055d74fb1
Arno Solo <arno756@outlook.com>
2026-04-28 10:07:47 +0800

    chore: increase HTTP request timeout to 40 seconds

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -5,7 +5,7 @@ import router from '../router'
 
 const http = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 10000,
+  timeout: 40_000,
   headers: {
     'Content-Type': 'application/json'
   }
