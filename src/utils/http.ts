84d238980a254aa725f02ef84799721f5b2eca52
Arno Solo <arno756@outlook.com>
2026-05-21 11:50:56 +0800

    feat: set timeout as 120s

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -5,7 +5,7 @@ import router from '../router'
 
 const http = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 60_000,
+  timeout: 120_000,
   headers: {
     'Content-Type': 'application/json'
   }
